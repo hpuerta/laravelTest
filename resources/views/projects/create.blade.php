6db64069c97305ee433efb465b058ad67d2b3999
Harry Puerta <hpuerta@gmail.com>
2019-04-23 20:41:33 -0500

    create project

---
+++ resources/views/projects/create.blade.php
@@ -0,0 +1,13 @@
+<h2>Create Project</h2>
+<form method="POST" action="/projects">
+	{{csrf_field()}}
+	<div>
+		<input type="text" name="title" placeholder="Project title">
+	</div>
+	<div>
+		<textarea name="description" placeholder="Project description"></textarea>
+	</div>
+	<div>
+		<button type="submit">Create project</button>
+	</div>
+</form>
